3129573b3e8f687986fe9e497422371975f27479
Robertodev10 <robertojrsilva@hotmail.com>
2023-07-15 19:56:30 -0300

    consulta SQL, projection, busca por lista

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -17,8 +17,9 @@ public class Game {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private  Long id;
 	private String title;
+	
 	@Column(name = "game_year")
-	private String year;
+	private Integer year;////
 	private String genre;
 	private String platforms;
 	private Double score;
@@ -35,7 +36,7 @@ public class Game {
 		
 	}
 
-	public Game(Long id, String title, String year, String genre,
+	public Game(Long id, String title, Integer year, String genre,
 			String platforms, Double score, String imgUrl,
 			String shortDescription, String longDescription) {
 		this.id = id;
@@ -65,11 +66,11 @@ public class Game {
 		this.title = title;
 	}
 
-	public String getYear() {
+	public Integer getYear() {
 		return year;
 	}
 
-	public void setYear(String year) {
+	public void setYear(Integer year) {
 		this.year = year;
 	}
 
